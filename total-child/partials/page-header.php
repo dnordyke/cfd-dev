a7e9407b40700ce9cb6f5f8f6a81edb4ab44321c
Unknown <david@simplydg.com>
2018-03-15 21:45:33 -0500

    adjusting blueribbon

--- total-child/partials/page-header.php
+++ total-child/partials/page-header.php
@@ -15,10 +15,20 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 } ?>
 <div id="blueRibbon">
-		<h3>July 20-29, 2018</h3>
-    <div class="countdown clr" data-countdown="2018/7/20" data-days="Days" data-hours="Hours" data-minutes="Minutes" data-seconds="Seconds"><?php do_shortcode ('[vc_column width="1/2" el_class="countdown"][vcex_countdown end_month="7" end_day="20" end_year="2018" days="Days" hours="Hours" minutes="Minutes"][/vc_column]');?></div>
-    <div class="textcenter theme-button-block-wrap theme-button-wrap clr"><a href="https://www.ticketmaster.com/" title="Buy Cheyenne Frontier Days Tickets" class="vcex-button theme-button align-center block buyTicketsBlue animate-on-hover wpex-data-hover" target="_blank" style="background:#2467ee;padding-top:15px;padding-right:35px;padding-bottom:16px;padding-left:35px;color:#ffffff;font-size:17px;font-weight:400;letter-spacing:0.75px;border-radius:0px;text-transform:capitalize;" data-hover-background="#4f84ef" data-hover-color="#ffffff"><span class="theme-button-inner">BUY TICKETS</span></a></div>
+	<h3>July 20-29, 2018</h3>
 
+		<div class="countdown">
+        <div class="vcex-module vcex-countdown-wrap">
+						<div class="vcex-countdown clr" data-countdown="2018/7/20" data-days="Days" data-hours="Hours" data-minutes="Minutes" data-seconds="Seconds"><?php do_shortcode ('[vc_column width="1/2" el_class="countdown"][vcex_countdown end_month="7" end_day="20" end_year="2018" days="Days" hours="Hours" minutes="Minutes"][/vc_column]');?>
+          	</div>
+       </div>
+    </div>
+
+	<div class="textcenter theme-button-block-wrap theme-button-wrap clr"
+			<a href="https://www.ticketmaster.com/" title="Buy Cheyenne Frontier Days Tickets" class="vcex-button theme-button align-center block buyTicketsBlue animate-on-hover wpex-data-hover" target="_blank" style="background:#2467ee;padding-top:15px;padding-right:35px;padding-bottom:16px;padding-left:35px;color:#ffffff;font-size:17px;font-weight:400;letter-spacing:0.75px;border-radius:0px;text-transform:capitalize;" data-hover-background="#4f84ef" data-hover-color="#ffffff">
+				<span class="theme-button-inner">BUY TICKETS</span>
+			</a>
+  </div>
 </div>
 <?php wpex_hook_page_header_before(); ?>
 <header class="<?php echo wpex_page_header_classes(); ?>">
